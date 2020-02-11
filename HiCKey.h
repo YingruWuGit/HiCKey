a1f92fac8f4e4d9aff9c2de6ffce373d8adf467a
YingruWuGit <arantilwu@gmail.com>
2020-02-11 18:18:17 -0500

    Add project files.

---
+++ HiCKey.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <deque>
+#include <map>
+#include <utility>
+#include <cstdlib>
+#include <numeric>
+#include <algorithm>
+
+
+class Hic {
+	const std::string fileName;
+	const std::string fileNameP;
+	const int cv;
+	double sv;
+	double hv;
+	std::vector<std::vector<std::pair<int, double>>> countMatrix;
+	std::vector<double> brownianP;
+public:
+	std::vector<int> cpI;
+	std::vector<int> cpS;
+	std::vector<double> pValue;
+
+	Hic(const char* _fileName, const char* _fileNameP, const int _cv, const double _sv, const double _hv = 0.0);
+	void topDown();
+	void topDown(int cpt0, int cpt1);
+	void testCp(int cp, int store);
+	void pruning();
+	void bottomUp();
+	void outPut();
+	std::vector<std::vector<double>> subMatrix(int s, int e);
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "HiCKey.h"
+//main C:/Users/Yingru/Source/Repos/hic/nijchr16_matrix.txt C:/Users/Yingru/Source/Repos/hic/BrownianP.txt 5 0.05 0.005
+
+int main(int argc, char* argv[]) {
+	int _cv = std::atoi(argv[3]);
+	double _sv = std::atof(argv[4]);
+	double _hv = 0.0;
+	if (argc > 5)
+		_hv = std::atof(argv[5]);
+
+	Hic sample(argv[1], argv[2], _cv, _sv, _hv);
+	sample.topDown();
+	sample.pruning();
+	sample.bottomUp();
+	sample.outPut();
+	return 0;
+}
